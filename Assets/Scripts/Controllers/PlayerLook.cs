82f4d09eff3a1a3a197090b84ea54092d82cfcf4
Porter <W0428713@campus.nscc.ca>
2021-10-29 10:39:49 -0300

    small update

--- Assets/Scripts/Controllers/PlayerLook.cs
+++ Assets/Scripts/Controllers/PlayerLook.cs
@@ -26,12 +26,11 @@ public class PlayerLook : MonoBehaviour
     void Update()
     {
         float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
-        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
 
-        xRotation -= mouseY;
-        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
 
-        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+            xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+            
         playerBody.Rotate(Vector3.up * mouseX);
     }
 }
