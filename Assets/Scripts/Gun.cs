dd6a85804bd397a23accca84db13c85f1d5df7fc
Porter <W0428713@campus.nscc.ca>
2021-11-03 09:00:16 -0300

    Fixed third person shooting

--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -5,7 +5,7 @@ public class Gun : MonoBehaviour
     public float damage = 10f;
     public float range = 100f;
 
-    public Camera fpsCam;
+    public CharacterController fpsPlayer;
     public ParticleSystem muzzleFlash;
     public GameObject impactEffect;
 
@@ -23,7 +23,7 @@ public class Gun : MonoBehaviour
         muzzleFlash.Play();
         
         RaycastHit hit;
-        if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
+        if (Physics.Raycast(fpsPlayer.transform.position, fpsPlayer.transform.forward, out hit, range))
         {
             Debug.Log(hit.transform.name);
 
